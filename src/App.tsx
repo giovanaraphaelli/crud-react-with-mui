51ec7c818cdf23f25b15855e34e3f71145291034
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-12-05 10:19:26 -0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+
 export const App = () => {
-  return <div>Teste</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
