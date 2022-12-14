76a01e43fc37535fe0a03bbf38763bf2c9e35e25
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-12-14 16:25:50 -0300

    feat: cria pagina de cidades

---
+++ src/pages/cidades/ListagemDeCidade.tsx
@@ -0,0 +1,27 @@
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { FerramentasDaListagem } from '../../shared/components';
+import { LayoutBase } from '../../shared/layouts';
+
+export const ListagemDeCidade: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
+  return (
+    <LayoutBase
+      titulo="Listagem de Cidades"
+      barraDeFerramentas={
+        <FerramentasDaListagem
+          textoBotaoNovo="Nova"
+          mostrarInputBusca
+          textoDaBusca={busca}
+          aoMudarTextoDeBusca={(texto) =>
+            setSearchParams({ busca: texto }, { replace: true })
+          }
+        />
+      }
+    ></LayoutBase>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
-import Home from '@mui/icons-material/Home';
 import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Dashboard } from '../pages';
+import { Dashboard, ListagemDeCidade } from '../pages';
 import { useDrawerContext } from '../shared/contexts';
+import Home from '@mui/icons-material/Home';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
 
 export const AppRoutes = () => {
   const { setDrawerOptions } = useDrawerContext();
@@ -10,12 +11,15 @@ export const AppRoutes = () => {
   useEffect(() => {
     setDrawerOptions([
       { label: 'Página Inicial', icon: <Home />, path: '/home' },
+      { label: 'Cidades', icon: <LocationCityIcon />, path: '/cidades' },
     ]);
   }, []);
 
   return (
     <Routes>
       <Route path="/home" element={<Dashboard />} />
+      <Route path="/cidades" element={<ListagemDeCidade />} />
+      {/* <Route path="/cidades/detalhe/:id" element={<ListagemDeCidade />} /> */}
 
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
