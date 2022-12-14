57859e53b940230cf77e94035c6d8ac067c5f507
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-12-14 16:58:34 -0300

    feat: add consulta para listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBase } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -10,7 +11,15 @@ export const ListagemDePessoas: React.FC = () => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
-  // useEffect(() => {}, []);
+  useEffect(() => {
+    PessoasService.getAll(1, busca).then((result) => {
+      if (result instanceof Error) {
+        alert(result.message);
+      } else {
+        console.log(result);
+      }
+    });
+  }, [busca]);
 
   return (
     <LayoutBase
