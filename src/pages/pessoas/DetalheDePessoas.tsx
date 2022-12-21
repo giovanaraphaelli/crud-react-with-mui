f60b7d696fbdbe8a964891ba7b047a5becb88554
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-12-21 12:15:08 -0300

    feat: add consulta e delete de pessoas

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,20 +1,50 @@
+import LinearProgress from '@mui/material/LinearProgress';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FerramentasDeDetalhe } from '../../shared/components';
 import { LayoutBase } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const DetalheDePessoas: React.FC = () => {
   const { id = 'nova' } = useParams<'id'>();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState('');
+
+  useEffect(() => {
+    if (id !== 'nova') {
+      setIsLoading(true);
+      PessoasService.getById(Number(id)).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+          navigate('/pessoas');
+        } else {
+          setNome(result.nomeCompleto);
+          console.log(result);
+        }
+      });
+    }
+  }, [id]);
 
   const handleSave = () => {
     console.log('Save');
   };
-  const handleDelete = () => {
-    console.log('Save');
+  const handleDelete = (id: number) => {
+    if (confirm('Deseja apagar o registro?')) {
+      PessoasService.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          alert('Registro apagado com sucesso!');
+          navigate('/pessoas');
+        }
+      });
+    }
   };
   return (
     <LayoutBase
-      titulo="Detalhe de Pessoa"
+      titulo={id === 'nova' ? 'Cadastro de Pessoa' : nome}
       barraDeFerramentas={
         <FerramentasDeDetalhe
           textoBotaoNovo="Nova"
@@ -24,7 +54,7 @@ export const DetalheDePessoas: React.FC = () => {
           mostrarBotaoApagar={id !== 'nova'}
           aoCLicarEmSalvar={handleSave}
           aoCLicarEmSalvarEFechar={handleSave}
-          aoCLicarEmApagar={handleDelete}
+          aoCLicarEmApagar={() => handleDelete(Number(id))}
           aoCLicarEmNovo={() => {
             navigate('/pessoas/detalhe/nova');
           }}
@@ -34,6 +64,7 @@ export const DetalheDePessoas: React.FC = () => {
         />
       }
     >
+      {isLoading && <LinearProgress variant="indeterminate" />}
       <p>DetalheDePessoas {id}</p>
     </LayoutBase>
   );
